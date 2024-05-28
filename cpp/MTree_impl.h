aecc1ffb06e29f2ec9f614ed924f118aa29a4afc
5imonk <konzett.simon@gmail.com>
2024-05-28 21:47:27 +0200

    removed comments / aknowl

--- cpp/MTree_impl.h
+++ cpp/MTree_impl.h
@@ -1,7 +1,3 @@
-// Copyright (c) 2020 CN Group, TU Wien
-// Released under the GNU Lesser General Public License version 3,
-// see accompanying file LICENSE or <https://www.gnu.org/licenses/>.
-
 #include <algorithm>
 #include <assert.h>
 #include <unordered_map>
--- cpp/Vector.h
+++ cpp/Vector.h
@@ -1,7 +1,3 @@
-// Copyright (c) 2020 CN Group, TU Wien
-// Released under the GNU Lesser General Public License version 3,
-// see accompanying file LICENSE or <https://www.gnu.org/licenses/>.
-
 #ifndef DSALMON_VECTOR_H
 #define DSALMON_VECTOR_H
 
