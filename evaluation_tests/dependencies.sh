302ed31f6a74afca6acd6573dea7994bda33b4a6
felixig <fiv@gmx.at>
2024-09-16 11:11:44 +0200

    Update dependencies.sh

--- evaluation_tests/dependencies.sh
+++ evaluation_tests/dependencies.sh
@@ -4,6 +4,5 @@ pip3 install matplotlib==3.7.5
 pip3 install scikit-learn==1.3.2
 pip3 install river==0.19.0
 pip3 install swig==4.2.1
-pip3 install dSalmon==0.1
 pip3 install cvi==0.4.0
 pip3 install optuna==3.2.0
