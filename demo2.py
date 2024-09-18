21b4c115173b1b2893f0d64739ff6bab80e36831
5imonk <konzett.simon@gmail.com>
2024-09-18 14:14:02 +0200

    working on film, and parameter expirements

--- demo2.py
+++ demo2.py
@@ -8,6 +8,50 @@ from sklearn.preprocessing import MinMaxScaler
 
 from scipy.io.arff import loadarff 
 
+def plotdata(x,y,t,pltname):
+
+    if x.shape[1]>1:
+        feats = np.sort(np.random.choice(x.shape[1], 2, replace=False))
+    else:
+        feats = np.sort(np.random.choice(x.shape[1], 2, replace=True))
+
+    x = x[:,feats]
+    # t = np.arange(len(y))
+    y = y.astype(int)
+    f0 = 'f'+str(feats[0])
+    f1 = 'f'+str(feats[1])
+
+    gs_kw = dict(width_ratios=[1, 2], height_ratios=[1, 1])
+    fig, axes = plt.subplot_mosaic([['left', 'upper right'],['left', 'lower right']], gridspec_kw=gs_kw, figsize=(10, 4), layout="constrained")
+    #for k, ax in axd.items():
+    #    annotate_axes(ax, f'axd[{k!r}]', fontsize=14)
+
+    #fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+    cmap = plt.get_cmap('tab20', len(np.unique(y)))
+    if np.sum(y==-1)==0:
+        axes['left'].scatter(x[:,0], x[:,1], s=5, c=y, alpha=0.7, cmap=cmap) # ,edgecolors='w'
+        axes['upper right'].scatter(t, x[:,0], s=5, c=y, alpha=0.7, cmap=cmap)
+        axes['lower right'].scatter(t, x[:,1], s=5, c=y, alpha=0.7, cmap=cmap)
+    else:
+        axes['left'].scatter(x[y>-1,0], x[y>-1,1], s=5, c=y[y>-1], alpha=0.7, cmap=cmap) # ,edgecolors='w'
+        axes['left'].scatter(x[y==-1,0], x[y==-1,1], s=5, c='black', alpha=0.7, cmap=cmap) # edgecolors='w'
+        axes['upper right'].scatter(t[y>-1], x[y>-1,0], s=5, c=y[y>-1], alpha=0.7, cmap=cmap)
+        axes['upper right'].scatter(t[y==-1], x[y==-1,0], s=5, c='black', alpha=0.7, cmap=cmap)
+        axes['lower right'].scatter(t[y>-1], x[y>-1,1], s=5, c=y[y>-1], alpha=0.7, cmap=cmap)
+        axes['lower right'].scatter(t[y==-1], x[y==-1,1], s=5, c='black', alpha=0.7, cmap=cmap)
+
+    axes['left'].set_ylabel(f0)
+    axes['left'].set_xlabel(f1)
+    axes['upper right'].set_ylabel(f0)
+    axes['lower right'].set_xlabel('time')
+    axes['lower right'].set_ylabel(f1)
+    axes['upper right'].yaxis.set_label_position("right")
+    axes['lower right'].yaxis.set_label_position("right")
+
+    # plt.savefig(pltname) 
+    # plt.close()
+    plt.show()
+
 # retrieve dataset from file into x,y arrays
 def load_data(filename):
 
@@ -109,52 +153,52 @@ norm = plt.Normalize(vmin=min(np.unique(p)), vmax=max(np.unique(p)))
 cmap_gt = plt.get_cmap('Dark2', len(np.unique(y)))
 
 # Plotting
-for obs_points, obs_labels, obs_t in zip(all_obs_points, all_obs_labels, all_obs_t):
-    points = np.array(obs_points)
-    labels = le.transform(np.array(obs_labels)) - 1
+# for obs_points, obs_labels, obs_t in zip(all_obs_points, all_obs_labels, all_obs_t):
+#     points = np.array(obs_points)
+#     labels = le.transform(np.array(obs_labels)) - 1
 
-    time_min = obs_t - T
-    time_max = obs_t + T
-    mask = (t >= time_min) & (t <= time_max)
-    filtered_points = x[mask]
-    filtered_labels = p[mask].astype(int)
-    filtered_gt_labels = y[mask].astype(int)
+#     time_min = obs_t - T
+#     time_max = obs_t + T
+#     mask = (t >= time_min) & (t <= time_max)
+#     filtered_points = x[mask]
+#     filtered_labels = p[mask].astype(int)
+#     filtered_gt_labels = y[mask].astype(int)
 
-    fig, axes = plt.subplots(1, 3, figsize=(18, 6))
+#     fig, axes = plt.subplots(1, 3, figsize=(18, 6))
     
-    # Plot filtered points from x (left plot)
-    scatter_filtered = axes[0].scatter(filtered_points[filtered_labels!=-1, 0], filtered_points[filtered_labels!=-1, 1], c=filtered_labels[filtered_labels!=-1], cmap=cmap, norm=norm, s=10, marker='.')
-    axes[0].scatter(filtered_points[filtered_labels==-1, 0], filtered_points[filtered_labels==-1, 1], 
-                c='black', s=10, marker='d', label='Outliers')
+#     # Plot filtered points from x (left plot)
+#     scatter_filtered = axes[0].scatter(filtered_points[filtered_labels!=-1, 0], filtered_points[filtered_labels!=-1, 1], c=filtered_labels[filtered_labels!=-1], cmap=cmap, norm=norm, s=10, marker='.')
+#     axes[0].scatter(filtered_points[filtered_labels==-1, 0], filtered_points[filtered_labels==-1, 1], 
+#                 c='black', s=10, marker='d', label='Outliers')
 
-    axes[0].set_title(f'Predictions at Time: {obs_t} +/- {T}')
-    axes[0].set_xlabel('Feature 0')
-    axes[0].set_ylabel('Feature 1')
-    axes[0].set_xlim(0, 1)
-    axes[0].set_ylim(0, 1)
+#     axes[0].set_title(f'Predictions at Time: {obs_t} +/- {T}')
+#     axes[0].set_xlabel('Feature 0')
+#     axes[0].set_ylabel('Feature 1')
+#     axes[0].set_xlim(0, 1)
+#     axes[0].set_ylim(0, 1)
 
-    scatter_obs = axes[1].scatter(points[:, 0], points[:, 1], c=labels, cmap=cmap, norm=norm, s=10, marker='x')
-    axes[1].set_title(f'Observers at Time: {obs_t}')
-    axes[1].set_xlabel('Feature 0')
-    axes[1].set_ylabel('Feature 1')
-    axes[1].set_xlim(0, 1)
-    axes[1].set_ylim(0, 1)
+#     scatter_obs = axes[1].scatter(points[:, 0], points[:, 1], c=labels, cmap=cmap, norm=norm, s=10, marker='x')
+#     axes[1].set_title(f'Observers at Time: {obs_t}')
+#     axes[1].set_xlabel('Feature 0')
+#     axes[1].set_ylabel('Feature 1')
+#     axes[1].set_xlim(0, 1)
+#     axes[1].set_ylim(0, 1)
 
-    scatter_gt = axes[2].scatter(filtered_points[filtered_gt_labels!=-1, 0], filtered_points[filtered_gt_labels!=-1, 1], c=filtered_gt_labels[filtered_gt_labels!=-1], cmap=cmap_gt, s=10, marker='.')
-    axes[2].scatter(filtered_points[filtered_gt_labels==-1, 0], filtered_points[filtered_gt_labels==-1, 1], 
-                c='black', s=10, marker='d', label='Outliers')
-    axes[2].set_title(f'Ground Truth at Time: {obs_t} +/- {T}')
-    axes[2].set_xlabel('Feature 0')
-    axes[2].set_ylabel('Feature 1')
-    axes[2].set_xlim(0, 1)
-    axes[2].set_ylim(0, 1)
+#     scatter_gt = axes[2].scatter(filtered_points[filtered_gt_labels!=-1, 0], filtered_points[filtered_gt_labels!=-1, 1], c=filtered_gt_labels[filtered_gt_labels!=-1], cmap=cmap_gt, s=10, marker='.')
+#     axes[2].scatter(filtered_points[filtered_gt_labels==-1, 0], filtered_points[filtered_gt_labels==-1, 1], 
+#                 c='black', s=10, marker='d', label='Outliers')
+#     axes[2].set_title(f'Ground Truth at Time: {obs_t} +/- {T}')
+#     axes[2].set_xlabel('Feature 0')
+#     axes[2].set_ylabel('Feature 1')
+#     axes[2].set_xlim(0, 1)
+#     axes[2].set_ylim(0, 1)
 
-    # axes[0].legend(*scatter_filtered.legend_elements(), title="Labels", loc="upper right")
-    # axes[1].legend(*scatter_obs.legend_elements(), title="Labels", loc="upper right")
-    # axes[2].legend(*scatter_gt.legend_elements(), title="Labels", loc="upper right")
+#     # axes[0].legend(*scatter_filtered.legend_elements(), title="Labels", loc="upper right")
+#     # axes[1].legend(*scatter_obs.legend_elements(), title="Labels", loc="upper right")
+#     # axes[2].legend(*scatter_gt.legend_elements(), title="Labels", loc="upper right")
 
-    plt.tight_layout()
-    plt.show()
+#     plt.tight_layout()
+#     plt.show()
 
 # fig = plt.figure(figsize=(15,4))
 # cmap = plt.get_cmap('tab20', len(np.unique(p)))
@@ -191,3 +235,6 @@ for i in range(3):
     ax.set_zlabel('f1')
 
 plt.show()
+
+
+plotdata(x,y,t,'gt_plot')
